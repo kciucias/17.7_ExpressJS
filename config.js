dd4dcbf37f38cacb9c7d6604e1a6093967b4c4d3
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-08-19 08:52:33 +0100

    finish

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '842601483184-qj1avg78dbe4058rjp4rd67si857d2ka.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'it_GS66lFRgHHyXmpBaW3rU2',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
